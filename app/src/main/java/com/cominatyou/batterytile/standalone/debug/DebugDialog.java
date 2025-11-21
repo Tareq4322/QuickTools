81e9042cfc106bf597f8e61099fe49c5ef8b02b2
Tareq <85187197+Tareq4322@users.noreply.github.com>
2025-11-21 19:33:52 +0600

    Update DebugDialog.java

--- app/src/main/java/com/cominatyou/batterytile/standalone/debug/DebugDialog.java
+++ app/src/main/java/com/cominatyou/batterytile/standalone/debug/DebugDialog.java
@@ -1,45 +1,31 @@
 package com.cominatyou.batterytile.standalone.debug;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.widget.Button;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AlertDialog;
-import androidx.core.content.res.ResourcesCompat;
 
 import com.cominatyou.batterytile.standalone.R;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
-import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class DebugDialog {
-    static ExecutorService executorService = null;
-
-    public static void show(final Context context) {
-        executorService = Executors.newFixedThreadPool(3);
-
-        final MaterialAlertDialogBuilder dialogBuilder = new MaterialAlertDialogBuilder(context)
+    public static void show(Context context) {
+        final ExecutorService executorService = Executors.newSingleThreadExecutor();
+        final AlertDialog dialog = new MaterialAlertDialogBuilder(context)
                 .setTitle(context.getString(R.string.debug_dialog_title))
                 .setMessage(context.getString(R.string.debug_dialog_initial_description))
                 .setView(R.layout.debug_dialog_layout)
+                .setNegativeButton(android.R.string.cancel, (dialogInterface, which) -> dialogInterface.dismiss())
                 .setCancelable(false)
-                .setNegativeButton(android.R.string.cancel, (dialog, which) -> {
-                    executorService.shutdownNow();
-                    dialog.dismiss();
-                })
-                .setOnDismissListener((dialogInterface) -> executorService.close());
+                .create();
 
-        final AlertDialog dialog = dialogBuilder.show();
-        ((TextView) Objects.requireNonNull(dialog.findViewById(android.R.id.message))).setTypeface(ResourcesCompat.getFont(context, R.font.gs_flex));
-        @SuppressLint("DiscouragedApi") int resId = context.getResources().getIdentifier("alertTitle", "id", context.getPackageName());
-        ((TextView) Objects.requireNonNull(dialog.findViewById(resId))).setTypeface(ResourcesCompat.getFont(context, R.font.gs_flex));
-        ((Button) Objects.requireNonNull(dialog.findViewById(android.R.id.button2))).setTypeface(ResourcesCompat.getFont(context, R.font.gs_flex));
-        ((Button) Objects.requireNonNull(dialog.findViewById(android.R.id.button1))).setTypeface(ResourcesCompat.getFont(context, R.font.gs_flex));
+        // FIX: Java 17 doesn't have .close() for ExecutorService.
+        // We use .shutdown() instead to kill the thread pool safely.
+        dialog.setOnDismissListener(dialogInterface -> executorService.shutdown());
 
+        dialog.show();
 
-        executorService.submit(() -> new DebugInfoCollector().collectDebugInfo(context, dialog, executorService));
+        executorService.execute(new DebugInfoCollector(context, dialog));
     }
 }
